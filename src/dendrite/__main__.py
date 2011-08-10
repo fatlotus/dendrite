ae70fc32def396677cabb7a1ad6790fc1d757fa5
Jeremy Archer <open-source@fatlotus.com>
2011-08-10 16:15:13 -0500

    Changed the default port for Dendrite to 1337.

--- src/dendrite/__main__.py
+++ src/dendrite/__main__.py
@@ -5,7 +5,7 @@ from twisted.internet import reactor
 (soft, hard) = resource.getrlimit(resource.RLIMIT_NOFILE)
 resource.setrlimit(resource.RLIMIT_NOFILE, (10000, hard))
 
-reactor.listenTCP(8080, dendrite.protocol.ServerFactory(), backlog=1024)
-# reactor.callLater(0.5, reactor.connectTCP, "0.0.0.0", 8080, dendrite.protocol.ClientFactory())
+reactor.listenTCP(1337, dendrite.protocol.ServerFactory(), backlog=1024)
+# reactor.callLater(0.5, reactor.connectTCP, "0.0.0.0", 1337, dendrite.protocol.ClientFactory())
 
 reactor.run()
