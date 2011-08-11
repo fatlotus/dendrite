a87ba9c83a977c6f4e8cd4c45398d3ebe015d37e
Jeremy Archer <open-source@fatlotus.com>
2011-08-11 13:34:23 -0700

    Production backfixes

--- src/dendrite/__main__.py
+++ src/dendrite/__main__.py
@@ -3,7 +3,8 @@ import resource
 from twisted.internet import reactor
 
 (soft, hard) = resource.getrlimit(resource.RLIMIT_NOFILE)
-resource.setrlimit(resource.RLIMIT_NOFILE, (10000, hard))
+# print "(soft=%i, hard=%i)" % (soft, hard)
+# resource.setrlimit(resource.RLIMIT_NOFILE, (10000, hard))
 
 reactor.listenTCP(1337, dendrite.protocol.ServerFactory(), backlog=1024)
 # reactor.callLater(0.5, reactor.connectTCP, "0.0.0.0", 1337, dendrite.protocol.ClientFactory())
