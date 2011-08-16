a974c37de7cc676728168ba22ace06cec6ccf73d
Jeremy Archer <open-source@fatlotus.com>
2011-08-16 16:50:02 -0500

    Added tests for the new dendrite protocol logic.

--- src/dendrite/tests/controllers/test_client.py
+++ src/dendrite/tests/controllers/test_client.py
@@ -9,41 +9,61 @@ class Controller(object):
    
    # Global callbacks
    def handle_protocol_error(self, error):
-      self.test.tearDown()
-      self.test.fail("Protocol error: %s" % error)
+      self.test.tests_complete("Protocol error: %s" % error)
    
    def handle_failure(self, sender, failure, message):
-      self.test.tearDown()
-      self.test.fail("Foreign error: %s" % message)
+      self.test.tests_complete("Foreign error: %s" % message)
    
    def connected(self, sender):
-      sender.login ('fred', 'fredspassword',
-         success=self.authenticated,
-         failure=self.authfailure,
+      def success(*vargs):
+         self.tests.tests_complete (
+            'Allowed to perform action without authentication.')
+      
+      def failure1(sender2, failure, message):
+         def failure2(sender3, failure, message):
+            sender.login ('fred', 'fredspassword',
+               success=self.authenticated,
+               failure=self.authfailure,
+            )
+            
+         sender.listen('', '',
+            failure=failure2,
+            success=success,
+            listen=success
+         )
+      
+      sender.fetch('GET', '', '', '',
+         failure=failure1,
+         data=success
       )
    
    # Fetch callbacks
    def fetched_data(self, sender, data):
       self.event('data')
       
-      self.test.tearDown()
+      self.test.tests_complete()
    
    # Authentication callbacks
    def authenticated(self, sender):
       if 'identify' not in self.events:
-         self.test.fail("Allowed entry without device identification.")
+         self.test.tests_complete("Allowed entry without device identification.")
       
       if 'auth-bad' not in self.events:
-         self.test.fail("Allowed phoney credentials into server.")
+         self.test.tests_complete("Allowed phoney credentials into server.")
       
       self.event('auth-ok')
       
-      sender.origin.fetch('GET', 'dendrite/aboutme', '', '', data=self.fetched_data,
-         failure=self.handle_failure)
+      sender.origin.fetch('GET', 'dendrite/aboutme', '', '',
+         data=self.fetched_data,
+         failure=self.handle_failure
+      )
    
    def authfailure(self, sender, failure, message):
+      if 'auth-bad' in self.events:
+         self.test.tests_complete("Blocked a valid (fake) user from the server.'")
+      
       self.event('auth-bad')
-      sender.origin.login ('fatlotus', 'test',
+      sender.origin.login ('test', 'test',
          success=self.authenticated,
          failure=self.authfailure,
       )
--- src/dendrite/tests/test_integration.py
+++ src/dendrite/tests/test_integration.py
@@ -1,21 +1,35 @@
 from twisted.internet import reactor
-import unittest
 from dendrite.protocol import base
 from dendrite.tests.controllers import test_client
+from dendrite.controllers import frontend
+from dendrite.backends import stubbackend
+import unittest
 
 class IntegrationTests(unittest.TestCase):
    def setUp(self):
       self.test_client = test_client.Controller(self)
-      
-   def tearDown(self):
+      self.frontend = frontend.Controller(stubbackend)
+   
+   def tests_complete(self, error=None):
       try:
          reactor.stop()
       except:
          pass
-   
-   def test_real_server(self):
-      fac = base.DendriteProtocol.build_factory(self.test_client, is_initiator=True)
       
-      reactor.connectSSL('heuristic.ci.uchicago.edu', 1337, fac, fac)
+      if error:
+         self.fail(error)
+   
+   # a is the initiator.
+   def integration_test(self, a, b):
+      facA = base.DendriteProtocol.build_factory(a, is_initiator=True)
+      facB = base.DendriteProtocol.build_factory(b, is_initiator=False)
+      
+      reactor.listenUNIX('tmp/integration_tests.sock', facB)
+      reactor.connectUNIX('tmp/integration_tests.sock', facA)
+      reactor.callLater(10, self.tests_complete, 'Test timed out.')
       
       reactor.run()
+      
+   
+   def test_all(self):
+      self.integration_test(self.test_client, self.frontend)
