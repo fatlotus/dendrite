fb552b7297efdcf38507500ca21b7e7aa53cd5d5
Jeremy Archer <open-source@fatlotus.com>
2011-08-18 14:56:59 -0500

    Removed legacy cruft from the __init__ files.

--- src/dendrite/protocol/__init__.py
+++
@@ -1,5 +0,0 @@
-from dendrite.protocol.low_level import DendriteServerProtocol as ServerProtocol
-from dendrite.protocol.messages import DendriteServerFactory as ServerFactory
-
-from dendrite.protocol.low_level import DendriteClientProtocol as ClientProtocol
-from dendrite.protocol.messages import DendriteClientFactory as ClientFactory
