df32dbb238c249d8d8589ddedae99065b7a3c5ea
Jeremy Archer <open-source@fatlotus.com>
2011-08-18 15:03:15 -0500

    Changed contstructor order to be more consistent, added comments, and added proper support for TLS.

--- src/dendrite/controllers/frontend.py
+++ src/dendrite/controllers/frontend.py
@@ -1,6 +1,6 @@
 # Primary dendrite protocol handler.
 
-from twisted.internet import defer
+from twisted.internet import defer, ssl
 import logging
 
 class Controller(object):
@@ -24,13 +24,43 @@ class Controller(object):
             pass
       return inner
    
-   # Constructors
-   def __init__(self, storage_backend, api_backend):
+   def __init__(self, api_backend, storage_backend):
+      """
+      Instantiate this frontend instance given an API and
+      generic storage backend. This method also initializes
+      the SSL context factory, which caches the certificate
+      and private key for the duration of the server 
+      instance lifecycle.
+      """
       self.storage_backend = storage_backend
       self.api_backend = api_backend
+      self.context_factory = ssl.DefaultOpenSSLContextFactory (
+         'config/keys/localhost.key',
+         'config/keys/localhost.crt',
+      )
    
-   # Global handlers
+   def provide_server_ssl_context(self):
+      """
+      Initializes a server-side SSL context for a newly-formed
+      connection. This method is called before #connected. In
+      this implementation, the code fetches the keys from
+      config/keys/server.key and config/keys/server.pem
+      """
+      
+      return self.context_factory.getContext()
+   
    def connected(self, sender):
+      """
+      This method is called by base.DendriteProtocol whenever a
+      new connection is made to this server. This method should
+      not be called by anything else, as the argument "sender"
+      is of an inner class of protocol.base.
+      
+      On a protocol level, this method queries the other side
+      with an "identify" and request and initializes the
+      connection.
+      """
+      
       # Initialize the per-connection session.
       sender.session['identcback'] = defer.Deferred()
       sender.session['backend_info'] = None
@@ -38,13 +68,19 @@ class Controller(object):
       # Default behaviour to require a identity before
       # any sort of request. Thus, send a request and only
       # then trigger the Deferred from it.
-      def got_identity(sender, userAgent, deviceID):
-         sender.session['identcback'].callback((userAgent, deviceID))
+      def got_identity(sender, user_agent, device_id):
+         sender.session['user_agent'] = user_agent
+         sender.session['device_id'] = device_id
+         sender.session['identcback'].callback(True)
       
       # Bother the client asynchronously.
       sender.identify(identity=got_identity)
       
-   def handle_login(self, sender, username, password):   
+   def handle_login(self, sender, username, password):
+      """
+      Handle a login request.
+      """
+      
       def try_authentication(identity):
          def authentication_success(session):
             sender.session['backend_info'] = session
@@ -66,6 +102,10 @@ class Controller(object):
    # protocol details in order to keep the code clean and understandable.
    @secure
    def handle_fetch(self, sender, method, url, query_string, body):
+      """
+      A secured API handler that triggers a REST request on the API
+      backend. See the protocol specification for details.
+      """
       
       session = sender.session['backend_info']
       req = self.api_backend.Request(session, method, url, query_string, body)
@@ -77,7 +117,13 @@ class Controller(object):
    
    @secure
    def handle_listen(self, sender, url, query_string):
+      """
+      A secured API handler that begins notifying the client of
+      updates to the specified REST request. The request type is
+      always GET, and the request body is empty.
       
+      See the specification for more details.
+      """
       session = sender.session['backend_info']
       req = self.api_backend.Request(session, method, url, query_string, body)
       
