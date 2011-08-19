488412cb21c90c245524de80174aff27c0161546
Jeremy Archer <open-source@fatlotus.com>
2011-08-19 15:27:32 -0500

    Added new container framework.

--- src/dendrite/container/__init__.py
+++ src/dendrite/container/__init__.py
@@ -1,9 +1,12 @@
 import stub
 import logging
 
-_instance = stub
-
 def instance():
+   if _instance is None:
+      from dendrite import storage, backends
+      
+      _instance = stub.Container(storage.choose_backend(), backends.choose_backend())
+   
    logging.warn("Deprecated singleton factory called for container instance.")
    
    return _instance
--- src/dendrite/container/memory.py
+++ src/dendrite/container/memory.py
@@ -1,14 +1,15 @@
-class Container():
-   def __init__(self, storageBackend, apiBackend):
+from dendrite import Component
+
+class Container(Component):
+   def __init__(self):
       """
-      Initializes this ServiceContainer, given a storage
-      backend. This container can be considered full of
+      Initializes this ServiceContainer.
+      
+      This container can be considered full of
       all of all possible services at instantiation, even
       if the actual implementation lazy-loads them.
       """
       self.services = { }
-      self.storageBackend = storageBackend
-      self.apiBackend = apiBackend
    
    def service(self, klass):
       """
@@ -20,8 +21,8 @@ class Container():
       # If we haven't already initialized this service,
       # then lazily do so and store it.
       #
-      if klass in self.services:
-         self.services[klass] = klass(self.storageBackend, self.apiBackend)
+      if klass not in self.services:
+         self.services[klass] = klass(self.storage_backend, self.api_backend)
       
       # Return the cached instance object.
       return self.services[klass]
