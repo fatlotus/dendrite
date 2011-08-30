adc83343e48d14a53b101d597663296e8afb6bf6
Jeremy Archer <open-source@fatlotus.com>
2011-08-30 11:15:47 -0500

    Changed the launch script to be written in Python.

--- src/dendrite/__main__.py
+++ src/dendrite/__main__.py
@@ -2,10 +2,10 @@ import dendrite.runner
 import yaml
 import sys
 
-def main():
+def main(*argv):
    configuration = yaml.load(open('config/dendrite.yaml'))
    
    return dendrite.runner.start_server(configuration)
 
 if __name__ == "__main__":
-   sys.exit(main())
+   sys.exit(main(sys.argv[1:]))
