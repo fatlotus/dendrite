64f13f048ebf7dec44b4ce4af59b0272a267db73
Jeremy Archer <open-source@fatlotus.com>
2011-08-30 11:03:42 -0500

    Put complete coverage in services/__init__.py

--- src/dendrite/services/__init__.py
+++ src/dendrite/services/__init__.py
@@ -1,6 +1,17 @@
 from dendrite.services import iphone
 
 def choose_service_for(userAgent):
+   """
+   Chooses the backend service for the specified user-agent string.
+   
+   >>> from dendrite import services
+   >>> services.choose_service_for('iPhone')
+   <class 'dendrite.services.iphone.Service'>
+   >>> services.choose_service_for('xx iphone xx')
+   <class 'dendrite.services.iphone.Service'>
+   >>> services.choose_service_for('xx Android xx')
+   >>>
+   """
    if 'IPHONE' in userAgent.upper():
       return iphone.Service
    else:
